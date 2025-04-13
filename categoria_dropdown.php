938a98f4fe44b349a94cbad5c5843de061879e8f
JoaoBird <joaofava38@gmail.com>
2025-04-13 00:58:10 -0300

    Incrementos de insercao

--- categoria_dropdown.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once 'carros_data.php';
-
-// Gerar o HTML para as categorias
-foreach($categorias as $categoria) {
-    echo "<a href=\"?filtro_categoria={$categoria}\">{$categoria}</a>";
-}
-echo "<a href=\"?limpar_filtros=1\">Limpar Filtros</a>";
-?>
